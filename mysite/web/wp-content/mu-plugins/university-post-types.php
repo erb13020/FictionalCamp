f48e10f19d6c8a7dfa0a2ca15c64470a2e6862ae
Eric Burt <eric.burt@protonmail.com>
2019-12-03 15:56:08 -0800

    Finish program post type

--- mysite/web/wp-content/mu-plugins/university-post-types.php
+++ mysite/web/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function camp_post_types() {
+	//events post type
   register_post_type('event', array(
   	'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'events'),
@@ -15,6 +16,39 @@ function camp_post_types() {
     ),
     'menu_icon' => 'dashicons-calendar'
   ));
+
+  //Program
+    register_post_type('program', array(
+  	'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add New Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
+    ),
+    'menu_icon' => 'dashicons-awards'
+  ));
+
+  //Activity
+    register_post_type('activity', array(
+  	'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'activity'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Activities',
+      'add_new_item' => 'Add New Activity',
+      'edit_item' => 'Edit Activity',
+      'all_items' => 'All Activities',
+      'singular_name' => 'Activity'
+    ),
+    'menu_icon' => 'dashicons-universal-access'
+  ));
+
 }
 
 add_action('init', 'camp_post_types');
